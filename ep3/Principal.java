0ffaf8e7bbc38d369511657b8dadb5f2d937dd54
Gabriel351 <gabriel.bbb.vga@hotmail.com>
2017-03-08 11:18:50 -0300

    exercício 3

---
+++ ep3/Principal.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Comparator;
+public class Principal {
+	public static void main(String arg[]) {
+		ArrayList<Aluno>alunos = new ArrayList<Aluno>(); 
+		Aluno aluno1 = new Aluno();
+		Aluno aluno2 = new Aluno();
+		Aluno aluno3 = new Aluno();
+		Aluno aluno4 = new Aluno();
+		Aluno aluno5 = new Aluno();
+		alunos.add(0,aluno1);
+		alunos.add(1,aluno1);
+		alunos.add(2,aluno1);
+		alunos.add(3,aluno1);
+		alunos.add(4,aluno1);
+		aluno1.setNome("Gabriel");
+		aluno1.setIdade(17);
+		aluno1.setNome("Luiz");
+		aluno1.setIdade(30);
+		aluno1.setNome("Agnaldo");
+		aluno1.setIdade(2);
+		aluno1.setNome("Maria");
+		aluno1.setIdade(5);
+		aluno1.setNome("João");
+		aluno1.setIdade(10);
+		Collections.sort(alunos);
+		for (int i = 0; i < alunos.size(); i++) {
+			System.out.println(alunos);
+		}
+}
