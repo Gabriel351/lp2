423f7c8b55c11010f544bfb4116658009476cfd7
alunos <gabriel.bbb.vga@hotmail.com>
2017-04-05 12:09:47 -0300

    Adicionado exercício do Singleton

---
+++ ep5/Singleton.java
@@ -0,0 +1,10 @@
+public class Singleton {
+	private static Singleton instancia;
+	private Singleton() {
+	}
+	public static Singleton getInstance() {
+		if (instancia == null) 
+			instancia = new Singleton();
+		return instancia;
+	}
+}
